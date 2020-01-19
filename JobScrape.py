2265978582ed7e68ed363dad5ebc91f930295dff
Eric Rivetna <erivetna87@gmail.com>
2020-01-18 21:43:52 -0600

    added libraries

---
+++ JobScrape.py
@@ -0,0 +1,20 @@
+"""
+!Python3 
+Eric Rivetna - Data Scientist 
+Lambda School Project: QuikHire.io
+
+Initial WebScraper for QuikHire.io - Indeed
+"""
+
+#Webscraping Libraries
+import requests
+import bs4
+from bs4 import BeautifulSoup
+
+#Data Libraries
+"""Check to remove unused Libraries"""
+import pandas as pd
+import numpy as np
+import time 
+import psycopg2
+
