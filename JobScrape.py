597683c31ac0e8269b5258a418efbdb440984762
Eric Rivetna <erivetna87@gmail.com>
2020-01-20 22:10:21 -0600

    added URL_Search function

--- JobScrape.py
+++ JobScrape.py
@@ -17,3 +17,24 @@ import pandas as pd
 import numpy as np
 import time 
 import psycopg2
+
+
+def url_search():
+    #Indeed uses + for any spacing between words to return results in URL
+    s = '+'
+    #Job Input Criteria
+    job_input = str(input('Enter Job Title Criteria: '))
+    job_input = job_input.split(' ')
+    job_input = s.join(job_input)
+
+    #City Criteria
+    city_input = str(input('Enter City Criteria: '))
+    job_input = job_input.split(' ')
+    job_input = s.join(job_input)
+
+
+
+    
+    # URL = 'https://www.indeed.com/jobs?q={}&l={}%2C+{}'.format(job_input, )
+
+url_search()
