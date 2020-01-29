d128709d29baff1561620605738641e9a36a3141
Eric Rivetna <erivetna87@gmail.com>
2020-01-28 22:31:29 -0600

    Selenium Scrape

--- JobScrape.py
+++ JobScrape.py
@@ -10,6 +10,8 @@ Initial WebScraper for QuikHire.io - Indeed
 import requests
 import bs4
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 #Data Libraries
 """Check to remove unused Libraries"""
@@ -22,38 +24,52 @@ import psycopg2
 
 
 def soup():
-    #Indeed uses + for any spacing between words to return results in URL
-    s = '+'
-    #Job Input Criteria
-    job_input = str(input('Enter Job Title Criteria: '))
-    job_input = job_input.split(' ')
-    job_input = s.join(job_input)
+    # #Indeed uses + for any spacing between words to return results in URL
+    # s = '+'
+    # #Job Input Criteria
+    # job_input = str(input('Enter Job Title Criteria: '))
+    # job_input = job_input.split(' ')
+    # job_input = s.join(job_input)
 
-    #City Criteria
-    city_input = str(input('Enter City Criteria: '))
-    city_input = city_input.split(' ')
-    city_input = s.join(city_input)
+    # #City Criteria
+    # city_input = str(input('Enter City Criteria: '))
+    # city_input = city_input.split(' ')
+    # city_input = s.join(city_input)
 
-    #State Criteria
-    state_input = str(input('Enter State Criteria: '))
-    state_input = state_input.split(' ')
-    state_input = s.join(state_input)
+    # #State Criteria
+    # state_input = str(input('Enter State Criteria: '))
+    # state_input = state_input.split(' ')
+    # state_input = s.join(state_input)
     
-    if job_input[-1:].endswith('+'):
-        job_input = job_input[:-1]
-    if city_input[-1:].endswith('+'):
-        city_input = city_input[:-1]
-    if state_input[-1:].endswith('+'):
-        state_input = state_input[:-1]
+    # if job_input[-1:].endswith('+'):
+    #     job_input = job_input[:-1]
+    # if city_input[-1:].endswith('+'):
+    #     city_input = city_input[:-1]
+    # if state_input[-1:].endswith('+'):
+    #     state_input = state_input[:-1]
     
-    URL = 'https://www.indeed.com/jobs?q={}&l={}%2C+{}'.format(job_input,city_input,state_input)
+    # URL = 'https://www.indeed.com/jobs?q={}&l={}%2C+{}'.format(job_input,city_input,state_input)
+
+    URL = 'https://www.indeed.com/jobs?q=Data+Science&l=Austin%2C+Texas'
     
     page = requests.get(URL)
     soup = BeautifulSoup(page.text, "html.parser")
     
-    return soup
+    return soup, URL
+
+def job_summary_sel():
+    
+    soup_func, URL = soup()
+
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get(URL)
+
+job_summary_sel()
+
+
 
 def job_title(soup):
+    print(soup.prettify())
     jobs = []
     for div in soup.find_all(name="div", attrs={"class":"row"}):
         for a in div.find_all(name="a", attrs={"data-tn-element":"jobTitle"}):
@@ -61,16 +77,29 @@ def job_title(soup):
     return jobs
 
 def company_name(soup):
-    company_names = []
-    for div in soup.find_all(name="div", attrs={"class:row"}):
-        company = div.find_all(name="span", attrs={"class":"company"})
-        if len(company) > 0:
-            for b in company:
-                company_names.append(b.text.strip())
-        else:
-            company_pull_2 = div.find_all(name="span", attrs={"class":"result-link-source"})
-            for span in company_pull_2:
-                company_names.append(span.text.strip())
-    return company_names
+    companies = []
+    for i in soup.find_all(name="div", attrs={"class":"row"}):
+        company = i.find_all(name="span", attrs={"class":"company"})
+        for a in company:
+            companies.append(a.text.strip())
     
-print(job_title(soup()))
+    return companies
+
+def location_name(soup):
+    locations = []
+    spans = soup.find_all(name="div", attrs={"class":"location"})
+    for g in spans:
+        locations.append(g.text.strip())
+    
+    return locations
+
+def summary(soup):
+    summaries = []
+    div = soup.find_all(name="div", attrs={"class":"summary"})
+    for li in div:
+        summaries.append(li.text.strip())
+    
+    return summaries
+
+
+
